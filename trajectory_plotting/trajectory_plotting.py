1b7d9bff8719bc5e0a628121383380a83dd4d084
Jonas Otto <jottosmail@gmail.com>
2022-03-24 19:42:42 +0100

    acceleration 2d plot

--- trajectory_plotting/trajectory_plotting.py
+++ trajectory_plotting/trajectory_plotting.py
@@ -10,6 +10,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def steerAnglesToTurnRate(vels, delta_fronts, delta_rears, track_length):
+    tanF = np.tan(np.array(delta_fronts))
+    tanR = np.tan(np.array(delta_rears))
+    steerSum = tanF + tanR
+    return np.array(vels) * (tanF - tanR) / np.sqrt(steerSum * steerSum + 4) / track_length
+
+
 class PlottingNode(Node):
 
     def __init__(self):
@@ -19,7 +26,8 @@ class PlottingNode(Node):
             '/trajectory_ilqr',
             self.listener_callback,
             10)
-        self.figure, ((self.ax_yaw, self.ax_vel), (self.ax_steer, self.ax_acc)) = plt.subplots(nrows=2, ncols=2)
+        self.figure, ((self.ax_yaw, self.ax_vel), (self.ax_steer, self.ax_acc), (self.acc_2d, _)) = plt.subplots(
+            nrows=3, ncols=2)
         plt.ion()
         plt.show()
 
@@ -30,11 +38,26 @@ class PlottingNode(Node):
 
         controls = msg.controls
         accs = [c.acc for c in controls]
-        delta_fronts = [c.delta_front * 180 / math.pi for c in controls]
-        delta_rears = [c.delta_rear * 180 / math.pi for c in controls]
+        delta_fronts_rad = [c.delta_front for c in controls]
+        delta_fronts = [c * 180 / math.pi for c in delta_fronts_rad]
+        delta_rears_rad = [c.delta_rear for c in controls]
+        delta_rears = [c * 180 / math.pi for c in delta_rears_rad]
 
         dt = Duration.from_msg(msg.delta_t).nanoseconds * 10 ** -9
         x = np.linspace(0, dt * len(states), num=len(states), endpoint=False)
+
+        lateralAccs = vels * steerAnglesToTurnRate(vels, delta_fronts_rad, delta_rears_rad, 0.2565)
+
+        self.acc_2d.clear()
+        self.acc_2d.set(adjustable='box', aspect='equal')
+        self.acc_2d.set_xlim([-5, 5])
+        self.acc_2d.set_ylim([-5, 5])
+        self.acc_2d.scatter(lateralAccs, accs)
+        circle = plt.Circle((0, 0), 2, fill=False)
+        self.acc_2d.add_patch(circle)
+        self.acc_2d.set_title("Acc [m/s²]")
+        self.acc_2d.set_ylabel("longitudinal")
+        self.acc_2d.set_xlabel("lateral")
 
         self.ax_yaw.clear()
         self.ax_yaw.plot(x, yaws)
@@ -43,17 +66,20 @@ class PlottingNode(Node):
 
         self.ax_vel.clear()
         self.ax_vel.plot(x, vels)
+        self.ax_vel.set_ylim([-2, 5])
         self.ax_vel.set_title("Vel")
         self.ax_vel.set_ylabel("m/s")
 
         self.ax_acc.clear()
         self.ax_acc.plot(x, accs)
+        self.ax_acc.set_ylim([-5, 5])
         self.ax_acc.set_title("Acc")
         self.ax_acc.set_ylabel("m/s²")
 
         self.ax_steer.clear()
         self.ax_steer.plot(x, delta_fronts, label="Front")
         self.ax_steer.plot(x, delta_rears, label="Rear")
+        self.ax_steer.set_ylim([-30, 30])
         self.ax_steer.set_title("Steer")
         self.ax_steer.set_ylabel("deg")
         self.ax_steer.legend()
